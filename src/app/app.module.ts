3d0f64e6c4167b7a77c236a51f9438090c48ac46
Karolina Kopanska <karolina@startup-werk.de>
2020-11-24 10:32:32 +0100

    Commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './start-page/start-page/start-page.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { DataBindingComponent } from './start-page/data-binding/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/start-page/data-binding/data-binding/data-binding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  public ClickTest="Hey bin ein Button";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
